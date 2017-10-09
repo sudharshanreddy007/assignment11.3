1edcca982b3eae490602aefdc42d07b266233a1a
sudharshanreddy007 <31057445+sudharshanreddy007@users.noreply.github.com>
2017-10-09 12:07:35 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class ExceptionDemo {
+
+	public static void main(String[]args){    //here it the main method
+		
+		void dosomething()
+		//in main method we have another method doSomething
+		{
+			
+			
+		void doMore()//one more method doMore
+		{
+			int dividend=50,divisor=0;//showing the division with zero
+			int divide=dividend/divisor;
+			System.out.println("the output is:"+divide);//now it shows error in line 
+		}
+	}
+	}
+}
